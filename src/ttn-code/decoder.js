0e63dbd3367ce593ec60eddc1131c4cf4d07ba06
Sosnus <sosnus15@gmail.com>
2019-01-08 09:04:15 +0100

    Update decoder.js

--- src/ttn-code/decoder.js
+++ src/ttn-code/decoder.js
@@ -21,11 +21,12 @@ function Decoder(bytes, port) {
   
   var flaga=0;
   
-  for (i=0;i<32;i++)
+  for (i=0;i<16;i++)
   {
     if ((String.fromCharCode(bytes[16+i]) != "0") | (flaga === 1))
     {
       decoded.Value += String.fromCharCode(bytes[16+i]);
+      // TODO: (String)  decoded.Value(HEX) convert to double, and write as string, mean 40355EB851EB851F(HEX)=21.37(Double)
       flaga=1;
     }
     
